305ecc55244aa41010ea6a1d559f25dde6acc38f
FarhanZizz <iamfarhanaziz@gmail.com>
2022-10-26 18:45:30 +0600

    routes setup finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog';
+import Courses from './components/Courses';
+import Faq from './components/Faq';
 import Header from './components/Header';
+import Login from './components/Login';
+import Main from './components/Main/Main';
+import Signup from './components/Signup';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <Signup></Signup>
+        },
+        {
+          path: 'courses',
+          element: <Courses></Courses>
+        },
+        {
+          path: 'blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: 'faq',
+          element: <Faq></Faq>
+        },
+      ]
+    }
+  ]);
   return (
     <div className="App">
-      <Header></Header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,37 +5,51 @@ import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import logo from '../assets/Logo.png';
 import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src={logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                    Tech ED
-                </Navbar.Brand>
+                <LinkContainer to='/'>
+                    <Navbar.Brand>
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Tech ED
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav className='d-flex align-items-center flex-nowrap'>
+                        <Nav className='d-flex align-items-center'>
                             <BsFillSunFill className='fs-5'></BsFillSunFill>
                             <Form.Check
                                 type="switch"
                                 id="custom-switch"
                                 className='mx-2'
-
                             />
                             <BsFillMoonFill></BsFillMoonFill>
                         </Nav>
-                        <Nav.Link href="#features">Courses</Nav.Link>
-                        <Nav.Link href="#pricing">FAQ</Nav.Link>
-                        <Nav.Link href="#pricing">Blog</Nav.Link>
+                        <LinkContainer to='/courses'>
+                            <Nav.Link>Courses</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/faq'>
+                            <Nav.Link>Faq</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/blog'>
+                            <Nav.Link>Blog</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/signup'>
+                            <Nav.Link>SignUp</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>

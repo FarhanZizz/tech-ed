eef1660bc983be79cf11c568a6359ce5e73def05
FarhanZizz <iamfarhanaziz@gmail.com>
2022-10-28 21:22:05 +0600

    added a pdf button to course details page

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import Pdf from "react-to-pdf";
 
 const CourseDetails = () => {
     const courseDetails = useLoaderData();
-    console.log(courseDetails)
+    const ref = React.createRef();
     return (
-        <Card>
-            <Card.Img className='w-25 mx-auto my-4' variant="top" src={courseDetails.image} />
+        <Card className='w-75 mx-auto my-5' ref={ref}>
+            <Card.Img className='w-50 mx-auto my-4' variant="top" src={courseDetails.image} />
             <Card.Body>
                 <Card.Title>{courseDetails.title}</Card.Title>
                 <Card.Text>
                     {courseDetails.description}
                 </Card.Text>
                 <Link to={`/checkout/${courseDetails.id}`} className='btn btn-primary' >Get premium Access</Link>
+                <Pdf targetRef={ref} filename="code-example.pdf">
+                    {({ toPdf }) => <button className='btn btn-primary m-2' onClick={toPdf}>Generate Pdf</button>}
+                </Pdf>
             </Card.Body>
         </Card >
     );
